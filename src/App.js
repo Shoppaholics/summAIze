7d6e5b3a326c444b9686453492ab0c268df35f8f
gladwintan <gladwintan@hotmail.sg>
2025-01-13 22:55:53 +0800

    Refactor route components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./components/Home.jsx";
-import SignIn from "./components/SignIn.jsx";
-import SignUp from "./components/SignUp.jsx";
+import Home from "./pages/Home.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
 const App = () => {
   return (
     <Router>
